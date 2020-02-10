34159f4da1043276b8bbfd019d0e102bd800297e
github NikolaCzv <github nikola.raicic@hotmail.com>
2020-02-10 14:38:11 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Router, Route} from 'react-router-dom'
 import './App.css';
+import history from './actions/history'
+import homepage from './components/homepage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router history={history}>
+        <Route exact path='/' component={homepage}/>
+      </Router>
     </div>
   );
 }
